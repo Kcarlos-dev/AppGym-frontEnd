a0648e248ccf08682821ae4a242b2214b9a6ef6e
Kcarlos <ca850075@gmail.com>
2025-03-27 01:59:18 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,105 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import styles from './page.module.css'
+import { useState } from "react";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+export default function Login() {
+    const [mostrarLogin, setMostrarLogin] = useState(true)
+    const [passwordSee, setPasswordSee] = useState({type:'password',see:true})
+    
+    function alternarLogin(text: string) {
+        setPasswordSee({type:'password',see:true})
+        if (text === "c") {
+            setMostrarLogin(false)
+            return
+        }
+        setMostrarLogin(true)
+    }
+    function visualizarSenha(){
+        if(passwordSee.see){
+            setPasswordSee({type:'text',see:false})
+            return 
+        }
+        setPasswordSee({type:'password',see:true})
+        return 
+
+    }
+
+    return (
+        <main className={styles.box_main}>
+            <div className={styles.box_secund}>
+                <div className={styles.style_box_medium}>
+                    <button onClick={() => alternarLogin("c")} className={`${mostrarLogin ? styles.style_btn_none_01 : styles.style_btn_none_02} text-black`}>CADASTRAR</button>
+                    <button onClick={() => alternarLogin('l')} className={`${mostrarLogin ? styles.style_btn_none_02 : styles.style_btn_none_01} text-black`}>LOGIN</button>
+                </div>
+                <div className={styles.style_box_perfi}>
+                    <i className="bi bi-person-circle"></i>
+                </div>
+
+                {mostrarLogin ? (
+                    <div>
+                        <div>
+                            <label>Email:</label>
+                            <br />
+                            <input className={styles.style_input} type="text" />
+                        </div>
+                        <div>
+                            <label>senha:</label>
+                            <br />
+                            <input className={styles.style_input} type={passwordSee.type} />
+                            <div className="d-flex">
+                                <input onClick={visualizarSenha} type="checkbox" />
+                                <label className="mx-1">Exibir senha</label>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="btn btn-link text-black p-0 py-1">esqueceu a senha?</button>
+                        </div>
+                        <div className={styles.style_flex_end}>
+                            <button  className={styles.style_btn_default + ' text-black'}>login</button>
+                        </div>
+                    </div>
+                ) : (
+                    <div >
+                        <div className={styles.box_scroll}>
+                            <div>
+                                <label>Nome:</label>
+                                <br />
+                                <input className={styles.style_input} type="text" />
+                            </div>
+                            <div>
+                                <label>CPF:</label>
+                                <br />
+                                <input className={styles.style_input} type="text" />
+                            </div>
+                            <div>
+                                <label>Data de Nascimento:</label>
+                                <br />
+                                <input className={styles.style_input} type="date" />
+                            </div>
+                            <div>
+                                <label>Email:</label>
+                                <br />
+                                <input className={styles.style_input} type="text" />
+                            </div>
+                            <div>
+                                <label>senha:</label>
+                                <br />
+                                <input className={styles.style_input} type={passwordSee.type} />
+                                <div className="d-flex">
+                                    <input onClick={visualizarSenha} type="checkbox" />
+                                    <label className="mx-1">Exibir senha</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.style_flex_end}>
+                            <button className={styles.style_btn_default + ' text-black'}>cadastras</button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </main>
+    )
 }
